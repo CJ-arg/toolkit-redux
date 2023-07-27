4b2c59890a838668599349d13d3451fcda0485f7
unknown <janoncarlos@gmail.com>
2023-07-27 18:08:47 -0300

    app bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { increment } from './store/slices/counter'
-function App() {
+import React from 'react'
+
+
+const App = () => {
 
   const { counter } = useSelector(state => state.counter)
   const dispatch = useDispatch()
@@ -12,12 +14,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+
       </div>
       <h1>Vite + React</h1>
       <div className="card">
