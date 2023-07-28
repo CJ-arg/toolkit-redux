62c25e524ac74741f7e7b8c5a874ebad7aae07c4
unknown <janoncarlos@gmail.com>
2023-07-28 10:32:57 -0300

    thunks

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 //pokeapi.co/api/v2/pokemon?limit=10&offset=0
@@ -5,7 +6,14 @@ export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
     // todo peticion htt
+    // const resp = await fetch(
+    // `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // )
+    // const data = await resp.json();
+    const { data } = await pokemonApi.get(
+      `pokemon?limit=10&offset=${page * 10}`
+    );
 
-    // dispatch(setPokemons)
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
